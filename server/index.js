33161f1ca005c3d0442cd229ca7623e46be3f719
wxaxiaoyao <765485868@qq.com>
2018-04-28 11:32:07 +0800

    update ip

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import { Nuxt, Builder } from 'nuxt'
 
 async function start () {
   const app = new Koa()
-  const host = process.env.HOST || '127.0.0.1'
+  const host = process.env.HOST || '0.0.0.0'
   const port = process.env.PORT || 7654
 
   // Import and Set Nuxt.js options
