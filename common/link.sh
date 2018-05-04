f651fc53a39412d22e389a0da3bc2202499c940e
wxaxiaoyao <765485868@qq.com>
2018-05-04 19:11:49 +0800

    add link.sh

---
+++ common/link.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln ../../kpapi/error.js error.js
+ln ../../kpapi/config.js config.js
+ln ../../kpapi/api/common.js api/common.js
+ln ../../kpapi/api/keepwork.js api/keepwork.js
+ln ../../kpapi/api/gitlab.js api/gitlab.js
+ln ../../kpapi/api/elasticSearch.js api/elasticSearch.js
